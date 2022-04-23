a0b7f0958263aa9dbfb0fc4f2c01d82f03f355c4
ghasemizahra <z.ghasemi.02018@gmail.com>
2022-04-23 22:53:16 +0430

    add

--- 2/src/App.js
+++ 2/src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import AbilitySection from './components/AbilitySection'
+import EditForm from './components/EditForm'
+import {useState} from 'react'
+import {Container, Grid, Box} from '@mui/material';
 
 function App() {
+  const [webDesign, setWebDesign] = useState([])
+  const [fronEnd, setFronEnd] = useState([])
+  const [backEnd, setBackEnd] = useState([])
+  const [edit, setEdit] = useState(false)
+
+  const handleClick = () => setEdit(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+        <Grid container justifyContent={'space-around'} rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{mt: 2}}>
+          <Grid item xs={6}>
+              <AbilitySection title="Web Design" handleClick={handleClick}/>
+              <AbilitySection title="Front-End" handleClick={handleClick}/>
+              <AbilitySection title="Back-End" handleClick={handleClick}/>
+          </Grid>
+          <Grid item xs={6}>
+              <EditForm edit={edit}/>
+          </Grid>
+      </Grid>
+      
+      
     </div>
   );
 }
